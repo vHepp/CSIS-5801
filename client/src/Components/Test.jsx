15a5af370d59a2afc25fdc379bdc756109d8e3b1
Rob <rfdidomenico@student.ysu.edu>
2022-09-22 12:01:15 -0400

    we put hi and x which equals 1

--- client/src/Components/Test.jsx
+++ client/src/Components/Test.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import '../Styles/Test.css'
 
 const Test = () => {
+	let x = 1
 	return (
 		<div className='test-body'>
 			Test
+			<p>
+				Hi {x}
+			</p>
 		</div>
 	)
 }
