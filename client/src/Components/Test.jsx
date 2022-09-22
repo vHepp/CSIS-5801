0d38ec7c44d3abf46ae724b183e968489025fe98
bwreeves <btorbyn@gmail.com>
2022-09-22 11:58:40 -0400

    This is the first push to my branch brenden

--- client/src/Components/Test.jsx
+++ client/src/Components/Test.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import '../Styles/Test.css'
 
 const Test = () => {
+	let x = 3;
+
 	return (
 		<div className='test-body'>
 			Test
+
+			<p>
+				{x}
+			</p>
 		</div>
 	)
 }
