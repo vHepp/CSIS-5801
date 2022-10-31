526ff736c733dcecd55e8589a1b6ff6bf56b2ed9
keith manjerovic <crmanjerovic@gmail.com>
2022-10-31 17:54:38 -0400

    Copied local working dev branch to keith

---
+++ client/src/Components/Pageheader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../Styles/Pageheader.css'
+
+
+const Pageheader = () => {
+	return (
+		<div className='pageheader-body'>
+			KeithBoard
+		</div>
+	)
+}
+
+export default Pageheader
