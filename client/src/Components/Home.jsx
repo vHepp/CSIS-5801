20f221feea7f26c55aea26586f85f173c989c64a
Rob <rfdidomenico@student.ysu.edu>
2022-11-01 11:49:37 -0400

    I added a bio in my profile page

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -5,7 +5,7 @@ import '../Styles/Home.css'
 const Home = () => {
 	return (
 		<div className='home-body'>
-			Home
+			Profile Settings
 		</div>
 	)
 }
--- client/src/Components/ProfilePage.jsx
+++ client/src/Components/ProfilePage.jsx
@@ -1,32 +1,41 @@
 import React,{useState} from 'react'
 import "../Styles/ProfilePage.css"
 
+import image from "../Images/4479.jpeg"
+
 const ProfilePage = () => {
-  const[email, setEmail, name, setName]= useState("Rob@Steveware.com")
+  const[email, setEmail]= useState("Rob@gmail.com")
+  const[name, setName]= useState("Rob DiDomenico")
+  const[bio, setBio]= useState("I love pizza.")
+
+
 
 
   return (
     <div className="ProfilePage"> 
         
-    <a href= "react" >ProfilePage</a>
-
-    <h2><label for="picture">Profile Picture </label>
+    <h1>Profile Picture </h1>
    
-    <img src="" picture="picture" /> </h2>
+    <img src={image} width="200px" height="250px" picture="picture" />
 
     <h2><label for="email">Your Email </label>
     <input email="email" value={email} onChange={
     (e)=>{setEmail(e.target.value)}
-    } placeholder= "Rob@Steveware.com" /></h2>
+    } placeholder= "Rob@gmail.com" /></h2>
 
     <h2><label for="name">Your Name </label>
     <input name="name"
     value={name} onChange={(e)=>{setName(e.target.value)}} placeholder= "Rob DiDomenico" /></h2>
 
-    <button type="update">Update </button>
+    <h2><label for="bio">Your bio/description </label>
+    
+    <textarea id="bio" name="message" rows="1" cols="50" value={bio} onChange={(e)=>{setBio(e.target.value)}} placeholder="I love pizza"></textarea></h2>
+
+    <button type="update">Update</button>
     
     <h1>{email}</h1>
     <h1>{name}</h1>
+    <h1>{bio}</h1>
 
 
      </div>
