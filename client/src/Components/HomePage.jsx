df2f64102bd9f8b4eb000a5b919f0d3fd978b9b1
Aaron Gorlitz <amgorlitz@student.ysu.edu>
2022-11-10 12:56:17 -0500

    Extra changes to the homepage with new formatting tools.

--- client/src/Components/HomePage.jsx
+++ client/src/Components/HomePage.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import { useState } from 'react'
-import '../Styles/HomePage.css'
-import Room from './Room.jsx'
-import InviteTest from './InviteTest.jsx'
+import React from "react";
+import { useState } from "react";
+import "../Styles/HomePage.css";
+import Room from "./Room.jsx";
+import InviteTest from "./InviteTest.jsx";
 import { RoomContext } from "../contexts/roomContext.js";
 
 const HomePage = () => {
@@ -14,78 +14,91 @@ const HomePage = () => {
 
   function addRoom() {
     if (rooms.length >= 3) {
-
-      setHomeError('Maximum number of rooms reached.');
+      setHomeError("Maximum number of rooms reached.");
       return;
     }
 
     console.table(rooms);
 
-    let name = document.getElementById('addRoomBtn').value;
+    let name = document.getElementById("addRoomBtn").value;
     if (name.length > 0 && !rooms.includes(name)) {
       setHomeError();
-      changeRooms(rooms => [...rooms, name]);
+      changeRooms((rooms) => [...rooms, name]);
       changeRoomCount(roomCount + 1);
       changeOpenRooms(openRooms - 1);
-    }
-    else {
+    } else {
       if (name.length <= 0) {
-
-        setHomeError('Room name cannot be blank.');
-      }
-      else {
-        setHomeError('That room name is already used.');
+        setHomeError("Room name cannot be blank.");
+      } else {
+        setHomeError("That room name is already used.");
       }
       return;
     }
   }
 
   return (
-    <div className='home-main'>
-      <div className='home-intro'>
-        <p>
-          Welcome to KeithBoard!
-        </p>
-        <p>
-          A interactive virtual classroom designed for
-          group communication.
-        </p>
+    <div className="home-main">
+      <div className="home-intro">
+        <p>Welcome to KeithBoard!</p>
+        <p>A interactive virtual classroom designed for group communication.</p>
         <p>
           Use the box below to name on open room. There is a limit of 3 rooms.
         </p>
         <div>
-          <input id="addRoomBtn" placeholder="Room Name" className='home-input'>
-          </input>
+          <input
+            id="addRoomBtn"
+            placeholder="Room Name"
+            className="home-input"
+          ></input>
           <p>
             <button className="button" onClick={addRoom}>
               Add Room
             </button>
           </p>
           <p id="home-error">{homeError}</p>
-          <div>
-            Room Count: {roomCount}
-          </div>
-          <div>
-            Open Rooms: {openRooms}
-          </div>
+          <div>Room Count: {roomCount}</div>
+          <div>Open Rooms: {openRooms}</div>
         </div>
         <div>
-          <div className='home-OpenedRooms'>
+          <div className="home-OpenedRooms">
             Opened Rooms:
-            <RoomContext.Provider value={{
-              rooms, changeRooms,
-              roomCount, changeRoomCount,
-              openRooms, changeOpenRooms
-            }}>
-              {rooms[0] && <Room name={rooms[0]} number={1} LinkToWebex={<InviteTest name={rooms[0]} />} />}
-              {rooms[1] && <Room name={rooms[1]} number={2} LinkToWebex={<InviteTest name={rooms[1]} />} />}
-              {rooms[2] && <Room name={rooms[2]} number={3} LinkToWebex={<InviteTest name={rooms[2]} />} />}
+            <RoomContext.Provider
+              value={{
+                rooms,
+                changeRooms,
+                roomCount,
+                changeRoomCount,
+                openRooms,
+                changeOpenRooms,
+              }}
+            >
+              {rooms[0] && (
+                <Room
+                  name={rooms[0]}
+                  number={1}
+                  LinkToWebex={<InviteTest name={rooms[0]} />}
+                />
+              )}
+              {rooms[1] && (
+                <Room
+                  name={rooms[1]}
+                  number={2}
+                  LinkToWebex={<InviteTest name={rooms[1]} />}
+                />
+              )}
+              {rooms[2] && (
+                <Room
+                  name={rooms[2]}
+                  number={3}
+                  LinkToWebex={<InviteTest name={rooms[2]} />}
+                />
+              )}
             </RoomContext.Provider>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
