30d31fdf6bd96648be84dd546ce17e3107f5df70
Aaron Gorlitz <amgorlitz@student.ysu.edu>
2022-11-22 11:49:40 -0500

    Recommitting changes to the footer page I made.

--- client/src/Components/footer.jsx
+++ client/src/Components/footer.jsx
@@ -3,5 +3,8 @@ import React from "react";
 <footer>
   <p> Creation credit goes to our team here at KeithBoard. </p>
   <p> This project is in honor of Kevin Snobnosky.</p>
-  <p>{"https://github.com/git/git-scm.com/blob/main/MIT-LICENSE.txt"}</p>
+  <p>
+    Our MIT License:
+    {"https://github.com/git/git-scm.com/blob/main/MIT-LICENSE.txt"}
+  </p>
 </footer>;
